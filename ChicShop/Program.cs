dab8db33e76a714eebe81e401e071482774aa7ff
Chic <chicfromfn@gmail.com>
2021-01-31 10:02:27 +0000

    e

--- ChicShop/Program.cs
+++ ChicShop/Program.cs
@@ -1,6 +1,10 @@
 using System;
 using System.Threading.Tasks;
+using System.Net.Http;
+using System.IO;
 using Fortnite_API;
+using Fortnite_API.Objects.V2;
+using SkiaSharp;
 
 namespace ChicShop
 {
@@ -13,9 +17,31 @@ namespace ChicShop
         {
             var api = new FortniteApi();
 
-            Console.WriteLine(api.V2.Cosmetics.GetBr("CID_222_Athena_Commando_F_DarkViking").Data.Name);
+            var shop = api.V2.Shop.GetBr().Data;
 
-            await Task.Delay(-1);
+            await Task.Delay(-1);        }
+
+        public SKBitmap DrawItem(BrCosmeticV2 item)
+        {
+            using (var cosmetic = GetBitmapFromUrl(item.Images.Featured))
+            {
+                using (var stream = File.OpenWrite(Path.GetDirectoryName(Assembly.GetExecitingAssembly().CodeBase)))
+                SKImage.FromBitmap(cosmetic).Encode(SKEncodedImageFormat.Png, 100).SaveTo()
+            }
+        }
+
+        public SKBitmap GetBitmapFromUrl(string url) => GetBitmapFromUri(new Uri(url));
+        public SKBitmap GetBitmapFromUrl(Uri url)
+        {
+            using (var client = new HttpClient())
+            {
+                var bytes = client.GetByteArrayAsync(url).Result;
+
+                using (var stream = new MemoryStream(bytes))
+                {
+                    return SKBitmap.Decode(stream);
+                }
+            }
         }
     }
 }
