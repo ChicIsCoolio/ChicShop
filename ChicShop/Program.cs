9546b14f9457abd31aba765a4ba784016a71735b
ChicIsCoolio <chicfromfn@gmail.com>
2021-01-31 09:40:44 +0100

    e

--- ChicShop/Program.cs
+++ ChicShop/Program.cs
@@ -1,17 +1,19 @@
 using System;
 using System.Threading.Tasks;
-using RestSharp;
+using Fortnite_API;
 
 namespace ChicShop
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
             => new Program().MainAsync(args).GetAwaiter().GetResult();
 
         public async Task MainAsync(string[] args)
         {
-            Console.WriteLine("Hello World but Async!");
+            var api = new FortniteApi();
+
+            Console.WriteLine(api.V2.Cosmetics.GetBr("CID_222_Athena_Commando_F_DarkViking").Data.Name);
 
             await Task.Delay(-1);
         }
