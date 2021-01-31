6ac452fa389f6b56932c48a4d1f695c74f61c0e4
Chic <chicfromfn@gmail.com>
2021-01-31 09:28:21 +0000

    Initial commit

---
+++ ChicShop/Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace ChicShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+            => new Program().MainAsync(args).GetAwaiter().GetResult();
+
+        public async Task MainAsync(string[] args)
+        {
+            Console.WriteLine("Hello World but Async!");
+
+            await Task.Delay(-1);
+        }
+    }
+}
